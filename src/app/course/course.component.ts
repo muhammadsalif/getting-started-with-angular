50e9a2716ad9785582760d33cc13db07593b93a9
Muhammad Salif <muhammad.salif@yahoo.com>
2023-06-04 23:44:20 +0500

    displayed list with decorator.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+  styleUrls: ['./course.component.scss'],
 })
 export class CourseComponent {
-
+  title = 'This is title of app';
+  courses = ['course1', 'course2', 'course3'];
 }
